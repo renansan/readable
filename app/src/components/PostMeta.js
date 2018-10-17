8ceaa8fc351af5047c9ea0d11157e221ed258316
Renan San <renaansan@gmail.com>
2018-10-17 01:46:56 -0300

    Connect part of PostMeta and PostLists to redux

--- app/src/components/PostMeta.js
+++ app/src/components/PostMeta.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import moment from 'moment'
 
 class PostMeta extends Component {
   render() {
+    const { id, category, author, timestamp } = this.props.post;
     return (
       <div className="post__meta">
-        <span className="post__categories">
-          <Link to="/category/cat-1" className="post__category">Category 1</Link>
+        {category.length ? (
+          <span className="post__categories">
+            <Link to={`/category/${category}`} className="post__category">{category}</Link>
+          </span>
+        ) : ''}
+        <span className="post__author">Posted by <b>{author}</b></span>
+        <span className="post__date">
+          <Link
+            to={ `/${category}/${id}` }
+            title={ moment(timestamp).format('MMM DD YYYY HH:mm') }>
+            {moment(timestamp).fromNow()}
+          </Link>
         </span>
-        <span className="post__author">Posted by <b>Renan</b></span>
-        <span className="post__date"><Link to="/post/post-1" title="13 Out 2018 23:33">a day ago</Link></span>
         <span className="post__comments">0 comments</span>
       </div>
     );
   }
 }
 
-export default PostMeta;
+const mapStateToProps = ({ posts }, { id }) => {
+  const post = posts.filter(item => item.id === id)[0];
+  return { post }
+};
+
+export default connect(mapStateToProps)(PostMeta);
--- app/src/components/PostsList.js
+++ app/src/components/PostsList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
 import PostMeta from '../components/PostMeta';
 import VoteScore from '../components/VoteScore';
 
@@ -15,30 +16,43 @@ class PostsList extends Component {
   }
 
   render() {
+    const { posts } = this.props;
     return (
       <section className="posts">
         <header className="posts__header">
-          <div className="posts__counter">10 posts</div>
+          <div className="posts__counter">{posts.length} posts</div>
           <select className="posts__order" value={this.state.order} onChange={this.handleChange}>
             <option value="score">Votes</option>
             <option value="recents">Recents</option>
             <option value="oldest">Oldest</option>
           </select>
         </header>
-        <div className="posts__list">
-          <article className="post posts__item">
-            <div className="post__info">
-              <h2 className="post__title"><Link to="/post/post-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt repellendus, vel suscipit consequatur nesciunt qui a placeat quod aspernatur harum quisquam pariatur asperiores at, veniam, natus fuga officia. Veritatis, consequuntur.</Link></h2>
-              <PostMeta />
-            </div>
-            <div className="post__score">
-              <VoteScore />
-            </div>
-          </article>
-        </div>
+        {!posts.length ? (
+          <span>No posts to show</span>
+        ) : (
+          <div className="posts__list">
+            {posts.map((post, index) => {
+              const meta = (({ timestamp, category, author }) => ({ timestamp, category, author }))(post);
+              return (
+                <article key={index} className="post posts__item">
+                  <div className="post__info">
+                    <h2 className="post__title"><Link to={`/${post.category}/${post.id}`}>{post.title}</Link></h2>
+                    <PostMeta id={post.id} data={meta} />
+                  </div>
+                  <div className="post__score">
+                    <VoteScore />
+                  </div>
+                </article>
+            )})}
+          </div>
+        )}
       </section>
     );
   }
 }
 
-export default PostsList;
+const mapStateToProps = ({ posts }) => {
+  return { posts }
+};
+
+export default connect(mapStateToProps)(PostsList);
