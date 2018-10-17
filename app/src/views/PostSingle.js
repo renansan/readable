d152b04f0e1dafe32f75fa8aaf6c5406aead3f19
Renan San <renaansan@gmail.com>
2018-10-17 02:59:02 -0300

    Remove PostDetails (incorporated to Post) and call Post directly in PostSingle

--- app/src/views/PostSingle.js
+++ app/src/views/PostSingle.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import PostDetails from '../components/PostDetails';
+import Post from '../components/Post';
+import PostsList from '../components/PostsList';
 
 class PostSingle extends Component {
   render() {
+    const { pathname } = this.props.location;
+    const id = pathname.substr(pathname.lastIndexOf('/') + 1);
+
     return (
-      <div className="post-details">
-        <PostDetails />
-      </div>
+      <section className="post-details">
+        <Post id={id} single={true} />
+
+        <div className="post__comments">
+          <PostsList parentId={id} />
+        </div>
+      </section>
     );
   }
 }
