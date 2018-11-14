33bbdd7a7193be365721fd5cd5da3c92c2226a01
Renan San <renaansan@gmail.com>
2018-11-14 01:52:32 -0200

    Remove defaultState

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -16,55 +16,7 @@ import {
   FETCH_CATEGORIES,
 } from '../actions'
 
-const initialState = {
-  posts: [
-    {
-      id: 'nkp7js6phap',
-      timestamp: '2018-10-16T03:40:46.964Z',
-      title: 'Post 3',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab nobis tempora magni harum, possimus numquam dicta sequi quod fugiat eligendi consequatur, molestias inventore iste dolore! Officia adipisci earum, reprehenderit pariatur!',
-      author: 'Author 3',
-      category: 'react',
-      voteScore: 0,
-      parentId: '',
-      postType: 'post',
-    },
-    {
-      id: 'd9y0jr0scef',
-      timestamp: '2018-10-17T03:30:37.465Z',
-      title: 'Post 2',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo dolorum asperiores quis similique, perspiciatis neque mollitia quas quibusdam vero corporis magnam voluptatem incidunt tenetur aperiam, commodi magni, a! Sunt, quisquam.',
-      author: 'Author 2',
-      category: 'redux',
-      voteScore: 0,
-      parentId: '',
-      postType: 'post',
-    },
-    {
-      id: 'q9etvq1yn4q',
-      timestamp: '2018-10-18T08:08:31.714Z',
-      title: 'Post',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus veritatis dicta quis aperiam. Sunt molestiae esse quibusdam sapiente, necessitatibus debitis, quia libero dolorem dicta. Velit quisquam atque blanditiis quas iste.',
-      author: 'Author',
-      category: 'udacity',
-      voteScore: 0,
-      parentId: '',
-      postType: 'post',
-    },
-  ],
-  comments: [
-    {
-      id: 1,
-      timestamp: '2018-10-18T22:00:00.714Z',
-      body: 'Message Comentário errado',
-      author: 'Author',
-      parentId: 'nkp7js6phap',
-      voteScore: 0
-    }
-  ],
-}
-
-function posts (state = initialState.posts, action) {
+function posts (state = [], action) {
   const { id, timestamp, title, body, author, category, voteScore, parentId, commentCount } = action;
 
   switch (action.type) {
